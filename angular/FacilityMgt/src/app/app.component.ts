77c515dbfbb27f330859b44a6dd06ee6601c7ff3
yousefaliq <yquran@mum.edu>
2020-03-12 17:20:20 -0500

    adding initial esri-map

--- angular/FacilityMgt/src/app/app.component.ts
+++ angular/FacilityMgt/src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'FacilityMgt';
+  // Set our map properties
+  mapCenter = [-91.9672633, 41.01768];
+  basemapType = 'satellite';
+  mapZoomLevel = 18;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log('The map loaded: ' + status);
+  }
 }
+
--- angular/FacilityMgt/src/app/app.module.ts
+++ angular/FacilityMgt/src/app/app.module.ts
@@ -1,21 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { MatSliderModule } from '@angular/material/slider';
+import { AppComponent } from "./app.component";
+import { EsriMapComponent } from "./esri-map/esri-map.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule
-  ],
+  declarations: [AppComponent, EsriMapComponent],
+  imports: [BrowserModule, MatSliderModule],
   providers: [],
   bootstrap: [AppComponent]
 })
