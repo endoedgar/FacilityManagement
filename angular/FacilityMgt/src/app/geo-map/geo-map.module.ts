083afe23e677ff9aebed1058e798545e92ea708c
yousefaliq <yquran@mum.edu>
2020-03-12 22:24:47 -0500

    added geo-map module

---
+++ angular/FacilityMgt/src/app/geo-map/geo-map.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EsriMapComponent } from './esri-map/esri-map.component';
+
+
+
+@NgModule({
+  declarations: [EsriMapComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    EsriMapComponent
+  ]
+})
+export class GeoMapModule { }
