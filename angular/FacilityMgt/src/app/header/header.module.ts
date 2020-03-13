3e7b67a55395b70f8c594e5d98622629f660d96d
yousefaliq <yquran@mum.edu>
2020-03-12 22:22:23 -0500

    added login and header modules

---
+++ angular/FacilityMgt/src/app/header/header.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainHeaderComponent } from './main-header/main-header.component';
+
+
+
+@NgModule({
+  declarations: [MainHeaderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
