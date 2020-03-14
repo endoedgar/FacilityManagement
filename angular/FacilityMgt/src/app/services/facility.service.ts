96e2e2ac433f206e26ded85a58ee4b7696a477bc
yousefaliq <yquran@mum.edu>
2020-03-14 18:51:12 -0500

    add facility service

---
+++ angular/FacilityMgt/src/app/services/facility.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class FacilityService {
+  private BASE_URL = "http://localhost:4000/api/v1";
+
+  constructor(private http: HttpClient) {}
+
+  getToken(): string {
+    return localStorage.getItem("token");
+  }
+
+  public addFacility(name: String, type: String, location: number[] ): Observable<any> {
+    console.dir( "new request ... ");
+    
+    return this.http.post(`${this.BASE_URL}/facilities/`, { name, type, location });
+  }
+
+}
