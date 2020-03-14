4cd4b6fd31c3e7339c400552cb76b5ab3cf07b86
yousefaliq <yquran@mum.edu>
2020-03-13 23:04:59 -0500

    added map-service

---
+++ angular/FacilityMgt/src/app/services/map-state.service.ts
@@ -0,0 +1,30 @@
+
+import { Injectable } from '@angular/core';
+import {MapStore} from '../map-store.class';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapStateService extends MapStore<any[]> {
+
+  getPoints(): Observable<__esri.Graphic[]> {
+    return this.getState();
+  }
+
+  addPoint(point: __esri.Graphic) {
+    const c = this.getValue();
+
+    if(typeof c !== 'undefined'){
+      this.setState([...this.getValue(), point]);  
+    }
+    else {
+      this.setState([point]);  
+    }
+
+  }
+  constructor() { 
+    // Important ;-) MapStore needs an initial value of any empty []
+    super([]);
+  }
+}
