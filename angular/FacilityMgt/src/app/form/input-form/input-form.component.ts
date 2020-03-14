90ba6f09e554e5be2d9219e03a1dc8f0612a1c9e
yousefaliq <yquran@mum.edu>
2020-03-13 23:05:45 -0500

    initial implementation of adding facilities

--- angular/FacilityMgt/src/app/form/input-form/input-form.component.ts
+++ angular/FacilityMgt/src/app/form/input-form/input-form.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { MapStateService } from '../../services/map-state.service';
+import { Observable, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-input-form',
@@ -8,14 +11,67 @@ import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class InputFormComponent implements OnInit {
  
-  control: FormControl;
-  control1: FormControl;
+  coordx: FormControl;
+  coordy: FormControl;
+  public points$: Observable<__esri.Graphic[]>
+  public list: Subscription;
+  public pMessage: any[] = [];
 
-  constructor(private fb: FormBuilder) {
-    this.control = fb.control({value: 'my val', disabled: false});
+  
+  constructor(public fb: FormBuilder, private msService: MapStateService, private changeDetect: ChangeDetectorRef) {
+    this.coordx = fb.control({value: 'my val', disabled: false});
+    this.coordy = fb.control({value: 'my val1', disabled: false});
   }
 
+
+  // If you want, you could also use this function to parse out the value
+  // and call this function from the component *cdkVirtualFor="let point of findPointValue(points$)"
+  public findPointValue(product: any) {
+    let finalValue: object;
+    if(product.source.value.length){
+      finalValue = product.source.value.map((val) => {
+        return [val.geometry.latitude + ", " + val.geometry.longitude]
+      });
+    }
+
+    return finalValue;
+  }
+
+
   ngOnInit(): void {
+    console.log("onInit")
+    // deactivate change detection component and children
+    this.changeDetect.detach();     
+    this.points$ = this.msService.getPoints();
+    this.list = this.points$.subscribe({
+      next: x => { 
+        let finalValue: any[];
+        if (x.length) {
+          finalValue = x.map((val:any) => {
+            return val.geometry.latitude + ", " + val.geometry.longitude;
+          });
+
+          // TODO: remove this testing code ...
+          const cdX = x.map((val:any) => {
+            return val.geometry.longitude;
+          });
+          const cdY = x.map((val:any) => {
+            return val.geometry.latitude;
+          });
+          this.coordx = this.fb.control({value: cdX, disabled: false});
+          this.coordy = this.fb.control({value: cdY, disabled: false});
+  
+        }
+        this.pMessage = finalValue;
+        this.changeDetect.detectChanges();
+      },
+      error: err => console.error('error in subscriber', err),
+      complete: () => console.log('complete')
+    })
+  }
+
+  ngOnDestroy(){
+    this.list.unsubscribe();
   }
 
 }
