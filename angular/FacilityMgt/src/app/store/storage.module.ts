3b48a65d85d7ae5774adbb62a619cb2d3afc8845
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-03-15 17:48:26 -0500

    Modularized App Routing

---
+++ angular/FacilityMgt/src/app/store/storage.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers } from './states/app.states';
+import { AuthEffects } from './effects/auth.effects';
+import { FacilityEffects } from './effects/facility.effects';
+import { UsersEffects } from './effects/users.effects';
+
+@NgModule({
+    imports: [
+        StoreModule.forRoot(reducers, {}), 
+        EffectsModule.forRoot([AuthEffects, FacilityEffects, UsersEffects])
+    ],
+    exports: [
+        StoreModule
+    ]
+})
+export class StorageModule {}
