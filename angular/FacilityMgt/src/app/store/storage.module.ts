7ffde3c7f28b66a128145c712a00680545e2a570
Edgar de Jesus Endo Junior <endoedgarjunior@gmail.com>
2020-03-16 00:53:25 -0500

    added @ngrx/store-devtools to debug redux store

--- angular/FacilityMgt/src/app/store/storage.module.ts
+++ angular/FacilityMgt/src/app/store/storage.module.ts
@@ -6,11 +6,14 @@ import { AuthEffects } from './effects/auth.effects';
 import { FacilityEffects } from './effects/facility.effects';
 import { UsersEffects } from './effects/users.effects';
 import { UIEffects } from './effects/ui.effects';
+import { environment } from 'src/environments/environment';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
     imports: [
         StoreModule.forRoot(reducers, {}), 
-        EffectsModule.forRoot([AuthEffects, FacilityEffects, UsersEffects, UIEffects])
+        EffectsModule.forRoot([AuthEffects, FacilityEffects, UsersEffects, UIEffects]),
+        environment.production ? [] : StoreDevtoolsModule.instrument()
     ],
     exports: [
         StoreModule
