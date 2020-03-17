c4be296fb793cae2266ec38870893ad0b0277397
dmahgoub <dmahgoub@miu.edu>
2020-03-17 13:06:02 -0500

    Dummy components edited | package.json updated

--- angular/FacilityMgt/src/app/components/dummyComponent/dummy.component.ts
+++ angular/FacilityMgt/src/app/components/dummyComponent/dummy.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dummyComponent',
-  template: '<h1>404 - Not Found dear</h1>',
+  template: '<h1>404 - Not Found, My Dears</h1>',
 })
 export class DummyComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
