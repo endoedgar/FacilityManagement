9c38714e17b8e309dd969937342b2c3b4ceed7e8
dmahgoub <dmahgoub@miu.edu>
2020-03-17 13:03:47 -0500

    Adding JWT | Populating Facility and User

--- express/routes/inspections.js
+++ express/routes/inspections.js
@@ -1,4 +1,5 @@
 const { Router, json } = require('express');
+const authenticateJWT = require('../middlewares/authenticateJWT')
 const Inspection = require('../schemas/inspection');
 
 const router = Router();
@@ -6,7 +7,11 @@ const router = Router();
 /* GET inspections listing */
 router.get('/', async (req, res, next) => {
     try {
-        res.status(200).send({ status: "success", data: await Inspection.find().populate("facility").populate({path: "inspector", select: "-password"}) });
+        res.status(200).send({
+            status: "success", data: await Inspection.find()
+                .populate("facility")
+                .populate({ path: "inspector", select: "-password" })
+        });
     } catch (err) {
         return next(err);
     }
@@ -15,7 +20,9 @@ router.get('/', async (req, res, next) => {
 /* GET one inspection */
 router.get('/:id', async (req, res, next) => {
     try {
-        const inspection = await Inspection.findOne({ "_id": req.params.id });
+        const inspection = await Inspection.findOne({ "_id": req.params.id })
+            .populate("facility")
+            .populate({ path: "inspector", select: "-password" });
         inspection ?
             res.status(200).send({ status: "success", data: inspection }) :
             res.status(404).send({ status: "failed", message: "Inspection not found." });
@@ -28,7 +35,7 @@ router.get('/:id', async (req, res, next) => {
 /* GET inspections of a facility */
 router.get('/facility/:facility_id', async (req, res, next) => {
     try {
-        const inspections = await Inspection.find({ "facility": req.params.facility_id });
+        const inspections = await Inspection.find({ "facility": req.params.facility_id }).populate("facility").populate({ path: "inspector", select: "-password" });
         inspections ?
             res.status(200).send({ status: "success", data: inspections }) :
             res.status(404).send({ status: "failed", message: "Inspections not found." });
@@ -39,7 +46,8 @@ router.get('/facility/:facility_id', async (req, res, next) => {
 
 
 /* POST one inspection */
-router.post('/', json(), async (req, res, next) => {
+router.post('/', authenticateJWT, json(), async (req, res, next) => {
+    req.body.inspector = req.user.id;
     await new Inspection(req.body).save((err, data) => {
         if (err) return next(err);
         res.status(201).json({ status: "success", message: "Created Successfully!", data });
@@ -48,8 +56,9 @@ router.post('/', json(), async (req, res, next) => {
 
 
 /* PATCH one inspection */
-router.patch('/:id', json(), async (req, res, next) => {
+router.patch('/:id', authenticateJWT, json(), async (req, res, next) => {
     try {
+        req.body.inspector = req.user.id;
         await Inspection.updateOne({ "_id": req.params.id }, { "$set": req.body });
         res.status(202).json({ status: "success", message: "Updated Successfully!" });
     } catch (err) {
@@ -59,8 +68,9 @@ router.patch('/:id', json(), async (req, res, next) => {
 
 
 /* DELETE one inspection */
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', authenticateJWT, async (req, res, next) => {
     try {
+        req.body.inspector = req.user.id;
         await Inspection.deleteOne({ "_id": req.params.id });
         res.status(202).json({ status: "success", message: "Deleted Successfully!" });
     } catch (err) {
